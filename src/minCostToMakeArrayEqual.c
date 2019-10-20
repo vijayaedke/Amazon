105c3332672f1cf18d0d973256a2b3570b32fbef
vijayaedke <707viee@gmail.com>
2019-10-20 23:35:27 +0530

    Added documentation to minCostToMakeArrayEqual.c

--- src/minCostToMakeArrayEqual.c
+++ src/minCostToMakeArrayEqual.c
@@ -1,3 +1,19 @@
+/*
+Given an array of integers where 1 \leq A[i] \leq 10^{18}. In one operation you can either Increment/Decrement any element by 1. The task is to find the minimum operations needed to be performed on the array elements to make all array elements equal.
+
+Examples:
+
+Input : A[] = { 1, 5, 7, 10 }
+Output : 11
+Increment 1 by 4, 5 by 0.
+Decrement 7 by 2, 10 by 5.
+New array A = { 5, 5, 5, 5 } with 
+cost of operations = 4 + 0 + 2 + 5 = 11.
+
+Input : A = { 10, 2, 20 }
+Output : 18
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<limits.h>
@@ -46,3 +62,11 @@ int main() {
    
    return 0;
 }
+
+/*
+input:
+{1, 6, 7, 10}
+output : 
+6	6	6	6	
+Operation cost = 10
+*/
